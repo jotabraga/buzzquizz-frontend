5d484caf71c6f48924b33aaee3eec8749ea51b49
jotabraga <jpmbraga.feg@gmail.com>
2021-04-22 15:29:41 -0300

    Implementação da coleta da array de objetos do tipo level

--- code/js/quizz-creation.js
+++ code/js/quizz-creation.js
@@ -3,10 +3,10 @@ let quizzPicture;
 let numberOfQuestions;
 let numberOfLevels;
 const questionsArray = [];
-
+const levelsArray = [];
 
 function creatTheQuizzQuestions(startCreationButton){
-    alert("teste");
+
     const startCreationScreen = startCreationButton.parentNode; 
     quizzTitle = startCreationScreen.children[0].value;
     quizzPicture = startCreationScreen.children[1].value;
@@ -16,6 +16,7 @@ function creatTheQuizzQuestions(startCreationButton){
 }
 
 function validationOfBasicInfo(title, picture, numOfQuestions, numOfLevels){
+
     if(title.length < 20 || title.length>65){
         alert("O titulo deve ter de 20 a 65 caracteres");
     }else if(isURL(picture) !== true){
@@ -30,6 +31,7 @@ function validationOfBasicInfo(title, picture, numOfQuestions, numOfLevels){
 }
 
 function isURL(str) {
+
     var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
@@ -40,6 +42,7 @@ function isURL(str) {
 }
 
 function changeToQuestionCreationScreen(){
+
     const pageInitial = document.querySelector(".quizz-creation-screen");
     pageInitial.classList.add("hidden");
     const pageToCreat = document.querySelector(".question-creation-screen");
@@ -48,13 +51,14 @@ function changeToQuestionCreationScreen(){
 }
 
 function questionsFillOut(){
+
     const questionsContent = document.querySelector(".fill-out-questions");
     questionsContent.innerHTML = "";
 
     for(let i=0; i < numberOfQuestions; i++){
         
         questionsContent.innerHTML += 
-        `<div class="question-box compacted" id="${i}"><div class="quizz-creation-instructions"><strong>Pergunta ${i+1}</strong><ion-icon name="create-outline" onclick="boxResizing(this)"></ion-icon></div>
+        `<div class="question-box compacted" id="q${i}"><div class="quizz-creation-instructions"><strong>Pergunta ${i+1}</strong><ion-icon name="create-outline" onclick="boxResizing(this)"></ion-icon></div>
             <input type="text" class="input-quizz-info" placeholder="Texto da pergunta"/>
             <input type="text" class="input-quizz-info" placeholder="Cor de fundo da pergunta"/>
             <div class="quizz-creation-instructions"><strong>Resposta correta</strong></div>
@@ -71,26 +75,28 @@ function questionsFillOut(){
 }
 
 function boxResizing(element){    
+
     const dadElement = element.parentNode;
     const boxToResizing = dadElement.parentNode;
     boxToResizing.classList.toggle("compacted");    
 }
 
-function getQuestionsInfo(element){
+function getQuestionsInfo(){
             
     for(let i=0; i < numberOfQuestions; i++){
 
-        const questionBox = document.getElementById(i);
+        let identificator = "q" + i;  
+        const questionBox = document.getElementById(identificator);
+        console.log(questionBox);
+        const answerArray = [];
 
-        for(let i=0; i < 3; i++){
-
-            const answerArray = [];
+        for(let i=0; i < 3; i++){           
 
             if(questionBox.children[3] !== null){
 
                 const texts = questionBox.children[3].value;
                 const picture = questionBox.children[4].value;
-                const rightAnswer;
+                let rightAnswer;
                 
                 if(i === 0){
                     rightAnswer = true;
@@ -115,7 +121,7 @@ function getQuestionsInfo(element){
         const questionObj = {
             title: titleOf,
             color: colorOf,
-            answer: aswerArray
+            answer: answerArray
         }
         questionsArray.push(questionObj);
     }
@@ -123,6 +129,7 @@ function getQuestionsInfo(element){
 }
 
 function changeToLevelsCreationScreen(){
+
     const pageInitial = document.querySelector(".question-creation-screen");
     pageInitial.classList.add("hidden");
     const pageToCreat = document.querySelector(".levels-creation-screen");
@@ -131,18 +138,51 @@ function changeToLevelsCreationScreen(){
 }
 
 function levelsFillOut(){
-    const questionsContent = document.querySelector(".fill-out-levels");
+
+    const levelsContent = document.querySelector(".fill-out-levels");
     levelsContent.innerHTML = "";
 
     for(let i=0; i < numberOfLevels; i++){
         
         levelsContent.innerHTML += 
-        `<div class="question-box compacted" id="l${i}"><div class="quizz-creation-instructions"><strong>Nível ${i+1}</strong><ion-icon name="create-outline" onclick="boxResizing(this)"></ion-icon></div>
+        `<div class="question-box compacted" id="${i}"><div class="quizz-creation-instructions"><strong>Nível ${i+1}</strong><ion-icon name="create-outline" onclick="boxResizing(this)"></ion-icon></div>
             <input type="text" class="input-quizz-info" placeholder="Título do nível"/>
             <input type="text" class="input-quizz-info" placeholder="% de acerto mínima"/>
             <input type="text" class="input-quizz-info" placeholder="URL da imagem do nível"/>
             <input type="text" class="input-quizz-info description-level" placeholder="Descrição do nível"/>`;
     }    
+}
+
+function getLevelsInfo(){
+
+   for(let i=0; i < numberOfLevels; i++){
+
+        let identificator = "l" + i;    
+        const levelBox = document.getElementById(identificator);
+
+        const titleOfLevel = levelBox.childen[1].value;
+        const imageOfLevel = levelBox.children[3].value;
+        const descriptionOfLevel = levelBox.children[4];
+        const minPercentOfHits = levelBox.children[2];
+
+        const levelObj = {
+            title: titleOfLevel,
+            image: imageOfLevel,
+            text: descriptionOfLevel,
+            minValue: minPercentOfHits
+        }
+
+        levelsArray.push(levelObj);      
+   }
+   changeToLevelsCreationScreen(); 
+}
+function changeToLevelsCreationScreen(){
+
+    const pageInitial = document.querySelector(".question-creation-screen");
+    pageInitial.classList.add("hidden");
+    const pageToCreat = document.querySelector(".levels-creation-screen");
+    pageToCreat.classList.remove("hidden");
+    levelsFillOut();
 }
 
 
