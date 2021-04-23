fcac0254fa2cefe91c81a1ce6e142216199cb77e
jotabraga <jpmbraga.feg@gmail.com>
2021-04-22 21:34:07 -0300

    Implementação das validações das questões por meio do forEach

--- code/js/quizz-creation.js
+++ code/js/quizz-creation.js
@@ -6,7 +6,7 @@ const questionsArray = [];
 const levelsArray = [];
 
 function creatTheQuizzQuestions(startCreationButton){
-    
+
     const startCreationScreen = startCreationButton.parentNode; 
     quizzTitle = startCreationScreen.children[0].value;
     quizzPicture = startCreationScreen.children[1].value;
@@ -38,7 +38,11 @@ function isURL(str) {
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    if(patern.test(str) === false){
+        alert("Insira uma URL valida");
+    }
     return pattern.test(str); 
+    
 }
 
 function changeToQuestionCreationScreen(){
@@ -87,18 +91,17 @@ function getQuestionsInfo(){
 
         let identificator = "q" + i;  
         const questionBox = document.getElementById(identificator);
-        console.log(questionBox);
         const answerArray = [];
 
-        for(let i=0; i < 3; i++){           
+        for(let i=3; i < 10; i+=2){           
 
-            if(questionBox.children[3] !== null){
+            if(questionBox.children[i] !== null){
 
-                const texts = questionBox.children[3].value;
-                const picture = questionBox.children[4].value;
+                const texts = questionBox.children[i].value;
+                const picture = questionBox.children[i+1].value;
                 let rightAnswer;
                 
-                if(i === 0){
+                if(i === 3){
                     rightAnswer = true;
                 } else {
                     rightAnswer = false
@@ -114,7 +117,7 @@ function getQuestionsInfo(){
 
             } else {
                 break;
-            }
+            }            
         }
         const titleOf = questionBox.children[1].value;
         const colorOf = questionBox.children[2].value;
@@ -125,15 +128,69 @@ function getQuestionsInfo(){
         }
         questionsArray.push(questionObj);
     }
-    changeToLevelsCreationScreen();             
+
+    validationOfQuestions(answersArray, questionsArray);
+
+    if(validationOfQuestions(answersArray, questionsArray)){
+        changeToLevelsCreationScreen();
+    } else {
+        alert("Preencha corretamente para continuar");
+    }      
+}
+
+function validationOfQuestions(answersArray, questionsArray){
+
+    const testLenght = questionsArray.forEach(verifyLenght(questionsArray));
+    const testHex = questionsArray.forEach(verifyHexadecimalColor(questionsArray));
+    const testNotNull = answerArray.forEach(verifyNotNull(answerArray));
+    const testURL = answerArray.forEach(isURL(answerArray.image));
+    const testNoQ = answerArray.forEach(verifyNumberOfQuestions(answerArray));
+
+    if(testLenght && testHex && testNotNull && testURL && testNoQ){
+        return true;
+    } else {
+        return false;
+    }    
+}
+function verifyLenght(element){
+    if(element.title.lenght < 20){
+        alert("O titulo deve conter 20 caracteres no minimo");
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function verifyHexadecimalColor(element){
+    const pattern = /#+([a-fA-F0-9]{6})/
+
+    if(element.colorOf.match(pattern)){
+        return true;
+    } else {
+        alert("Insira uma cor hexadecimal");
+        return false;
+    }
+}
+function verifyNotNull(element){
+    if(element.text !== null){
+        return true;
+    } else{
+        alert("O texto da resposta não pode estar em branco");
+        return false;
+    }
+}
+function verifyNumberOfQuestions(element){
+    if(element.length >== 2){
+        return true;
+    }
 }
 
 function changeToLevelsCreationScreen(){
 
-    const pageInitial = document.querySelector(".question-creation-screen");
-    pageInitial.classList.add("hidden");
-    const pageToCreat = document.querySelector(".levels-creation-screen");
-    pageToCreat.classList.remove("hidden");
+    const pageToHide = document.querySelector(".question-creation-screen");
+    pageToHide.classList.add("hidden");
+    const pageToShow = document.querySelector(".levels-creation-screen");
+    pageToShow.classList.remove("hidden");
     levelsFillOut();
 }
 
@@ -155,7 +212,7 @@ function levelsFillOut(){
 
 function getLevelsInfo(){
 
-   for(let i=0; i < numberOfLevels; i++){
+  /* for(let i=0; i < numberOfLevels; i++){
 
         let identificator = "l" + i;    
         const levelBox = document.getElementById(identificator);
@@ -173,12 +230,13 @@ function getLevelsInfo(){
         }
 
         levelsArray.push(levelObj);      
-   }
-   pushArrayOfQuizzToServer();
-   changeToLevelsCreationScreen(); 
+    } */
+    finalQuizzFillOut();
+  // pushArrayOfQuizzToServer();
+   
 }
 
-function pushArrayOfQuizzToServer(){
+/*function pushArrayOfQuizzToServer(){
 
     const quizzObj = {
         title: quizzTitle,
@@ -186,16 +244,38 @@ function pushArrayOfQuizzToServer(){
         questions: questionsArray,
         levels: levelsArray
     }
+    console.log(quizzObj);
     const requisition = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes",quizzObj);    
 }
+*/
 
 
-function changeToLevelsCreationScreen(){
-
-    const pageInitial = document.querySelector(".levels-creation-screen");
-    pageInitial.classList.add("hidden");
-    const pageToCreat = document.querySelector(".last-quizz-screen");
-    pageToCreat.classList.remove("hidden");
+function finalQuizzFillOut(){
+    const imgQuizz = document.querySelector(".img-quizz");
+    imgQuizz.innerHTML += `<div style="background: linear-gradient(to bottom, transparent, #000)>
+    <div class="quizz-description">${quizzTitle}</div></div>`;
+    imgQuizz.style.backgroundImage = `url(${quizzPicture})`;
+    changeToLastScreen()
 }
+
+function changeToLastScreen(){
+
+    const pageToHide = document.querySelector(".levels-creation-screen");
+    pageToHide.classList.add("hidden");
+    const pageToShow = document.querySelector(".last-quizz-screen");
+    pageToShow.classList.remove("hidden");
+
+
+   /* const quizzObj = {
+        title: quizzTitle,
+        image: quizzPicture,
+        questions: questionsArray,
+        levels: levelsArray
+    }
+    console.log(quizzObj);
+*/
+   
+}
+
 
 
